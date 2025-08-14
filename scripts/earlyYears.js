d29078b853696841cd516ae5007c67434f4baba2
German <germankrajcar@gmail.com>
2025-08-14 11:47:12 -0300

    remove old navbar

--- scripts/earlyYears.js
+++ scripts/earlyYears.js
@@ -1,10 +1,6 @@
 import { CONTENT_EARLY_YEARS_ENDPOINT } from './apiConstants.js'
 import { loadContentPage } from './content.js';
 
-/**
- * DOMContentLoaded event listener to load numeric and key facts sections, 
- * loading data from the API
- */
 document.addEventListener("DOMContentLoaded", () => {
     loadContentPage(CONTENT_EARLY_YEARS_ENDPOINT, 'early-years-content');
 });
